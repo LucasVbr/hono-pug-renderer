e1353c268cd424b25ef207b64976041e05d42148
Lucàs <86352901+LucasVbr@users.noreply.github.com>
2025-05-29 09:51:34 +0200

    feat: add build and CI configuration files

---
+++ build.ts
@@ -0,0 +1,26 @@
+import type { BuildConfig } from 'bun'
+import dts from 'bun-plugin-dts'
+
+const defaultBuildConfig: BuildConfig = {
+  entrypoints: ['./src/index.ts'],
+  outdir: './dist',
+  external: [
+    'hono',
+    'pug',
+    'path',
+  ]
+}
+
+await Promise.all([
+  Bun.build({
+    ...defaultBuildConfig,
+    plugins: [dts()],
+    format: 'esm',
+    naming: "[dir]/[name].js",
+  }),
+  Bun.build({
+    ...defaultBuildConfig,
+    format: 'cjs',
+    naming: "[dir]/[name].cjs",
+  })
+])
